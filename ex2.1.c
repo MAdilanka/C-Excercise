55040db8958aefafd3f73d7792c010b0aa09d93e
MAdilanka <dilankamallawaarachchi94@gmail.com>
2018-06-19 18:04:52 +0530

    this is c program

---
+++ ex2.1.c
@@ -0,0 +1,12 @@
+/* program-2.4 */
+#include <stdio.h>
+int main ()
+{
+ printf("University Of Moratuwa,\n");
+ printf("Katubedda,\n");
+ printf("Moratuwa,\n");
+ printf("Srilankan\n");
+ printf("---------n\n");
+ printf("www.mrt.ac.lk\n");
+ return 0;
+}
