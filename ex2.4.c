bfc927103a2144f701b2dac5e8a427112a485154
MAdilanka <dilankamallawaarachchi94@gmail.com>
2018-06-20 16:19:57 +0530

    2.4

---
+++ ex2.4.c
@@ -0,0 +1,14 @@
+/* exercise-2.4 */
+#include<stdio.h>
+int main ()
+{
+   int a= 3;
+   float b = 10.0;
+   float c ;
+   c = b/a;
+   printf("A is %d\n",a);
+   printf("B is %f\n",b);
+   printf("Answer Is %.2f\n",c);
+   return 0;
+}
+
